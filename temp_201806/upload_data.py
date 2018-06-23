2517402c45e1911cff2ccedf3bbe41c1716df6f8
yishantao <38241025+yishantao@users.noreply.github.com>
2018-06-23 22:18:01 +0800

    fix

--- temp_201806/upload_data.py
+++ temp_201806/upload_data.py
@@ -46,8 +46,8 @@ if __name__ == '__main__':
     data = read_excel('student_num.xlsx')
     database_pool.runInteraction(insert_table, data['S_NUM'])
     # 使用reactor.callLater函数定时执行函数，第一个参数为等待的秒数，第二个参数为需要调用的函数
-    reactor.callLater(2, reactor.stop)
-    reactor.run()
+    # reactor.callLater(2, reactor.stop)
+    # reactor.run()
 
 """说明：其实可以不用这么写，我写的时候考虑数据量庞大的话可以使用多线程机制；虽然在Python中多线程机制有点鸡肋，
 但Twisted提供了以异步方式多线程访问数据库的模块adbapi，我觉得使用该模块可以显著提高程序访问数据库的效率；
