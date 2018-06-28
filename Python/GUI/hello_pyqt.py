afe2b3addea39fe46c1877bf18098106b73382ed
yishantao <38241025+yishantao@users.noreply.github.com>
2018-06-28 16:50:40 +0800

    feat: PyQT

---
+++ Python/GUI/hello_pyqt.py
@@ -0,0 +1,16 @@
+# -*-coding:utf-8-*-
+import sys
+from PyQt5 import QtCore, QtGui
+
+
+class MyWindow(QtGui.QWindow):
+    def __init__(self):
+        QtGui.QWindow.__init__(self)
+        self.setWindowTitle('PyQT')
+        self.resize(300, 200)
+
+
+app = QtGui.QGuiApplication(sys.argv)
+mywindow = MyWindow()
+mywindow.show()
+app.exec_()
