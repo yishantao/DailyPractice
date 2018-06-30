bca16ab94b353540e82400f946bce91803f1227e
yishantao <38241025+yishantao@users.noreply.github.com>
2018-06-30 08:29:57 +0800

    refactor

--- Python/GUI/hello_pyqt.py
+++ Python/GUI/hello_pyqt.py
@@ -1,17 +1,20 @@
 # -*-coding:utf-8-*-
-""""""
+"""创建窗口"""
+
 import sys
-from PyQt5 import QtCore, QtGui
+from PyQt5 import QtWidgets
 
 
-class MyWindow(QtGui.QWindow):
+# 通过继承QtWidgets.QMainWindow创建类
+class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        QtGui.QWindow.__init__(self)
-        self.setWindowTitle('PyQT')
-        self.resize(300, 200)
+        QtWidgets.QMainWindow.__init__(self)
+        self.setWindowTitle('PyQT')  # 设置窗口标题
+        self.resize(300, 200)  # 设置窗口大小
 
 
-app = QtGui.QGuiApplication(sys.argv)
-mywindow = MyWindow()
-mywindow.show()
-app.exec_()
+app = QtWidgets.QApplication(sys.argv)  # 创建QApplication对象
+my_window = MyWindow()
+my_window.show()  # 显示窗口
+# exec_进入消息循环，exit确保应用程序的退出
+sys.exit(app.exec_())
