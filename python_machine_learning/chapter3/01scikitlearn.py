0c35ae935b8454ab94d65f23027842efdc66393f
yishantao <38241025+yishantao@users.noreply.github.com>
2018-07-07 20:04:30 +0800

    feat: python machine learning

---
+++ python_machine_learning/chapter3/01scikitlearn.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+"""This module is used to test groupby"""
+
+import numpy as np
+from sklearn import datasets
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+from sklearn.linear_model import Perceptron
+
+ppn = Perceptron(max_iter=40, eta0=0.1, random_state=0)
+ppn.fit(X_train_std, y_train)
+
+y_pred = ppn.predict(X_test_std)
+print('Misclassified samples: %d' % (y_test != y_pred).sum())
+
+from sklearn.metrics import accuracy_score
+
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
