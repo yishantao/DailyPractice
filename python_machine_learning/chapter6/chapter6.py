786771e5d131e8421295448ffc6bc5b75a947a5f
yishantao <38241025+yishantao@users.noreply.github.com>
2018-07-09 17:10:53 +0800

    feat: validation curve

--- python_machine_learning/chapter6/chapter6.py
+++ python_machine_learning/chapter6/chapter6.py
@@ -23,8 +23,8 @@ from sklearn.pipeline import make_pipeline
 
 pipe_lr = make_pipeline(StandardScaler(), PCA(n_components=2), LogisticRegression(random_state=1))
 
-pipe_lr.fit(X_train, y_train)
-y_pred = pipe_lr.predict(X_test)
+# pipe_lr.fit(X_train, y_train)
+# y_pred = pipe_lr.predict(X_test)
 # print('Test Accuracy: %.3f' % pipe_lr.score(X_test, y_test))
 
 # k折交叉验证
@@ -42,14 +42,107 @@ import numpy as np
 #
 # print('\nCV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
 
+# 分层k折交叉验证
+# from sklearn.model_selection import cross_val_score
+#
+# scores = cross_val_score(estimator=pipe_lr,
+#                          X=X_train,
+#                          y=y_train,
+#                          cv=10,
+#                          n_jobs=1)
+# print('CV accuracy scores: %s' % scores)
+# print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
 
-from sklearn.model_selection import cross_val_score
+# 学习曲线函数评估模型
+import matplotlib.pyplot as plt
+from sklearn.model_selection import learning_curve
 
-scores = cross_val_score(estimator=pipe_lr,
-                         X=X_train,
-                         y=y_train,
-                         cv=10,
-                         n_jobs=1)
-print('CV accuracy scores: %s' % scores)
-print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+pipe_lr = make_pipeline(StandardScaler(),
+                        LogisticRegression(penalty='l2', random_state=1))
 
+train_sizes, train_scores, test_scores = learning_curve(estimator=pipe_lr,
+                                                        X=X_train,
+                                                        y=y_train,
+                                                        train_sizes=np.linspace(0.1, 1.0, 10),
+                                                        cv=10,
+                                                        n_jobs=1)
+
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(train_sizes, train_mean,
+         color='blue', marker='o',
+         markersize=5, label='training accuracy')
+
+plt.fill_between(train_sizes,
+                 train_mean + train_std,
+                 train_mean - train_std,
+                 alpha=0.15, color='blue')
+
+plt.plot(train_sizes, test_mean,
+         color='green', linestyle='--',
+         marker='s', markersize=5,
+         label='validation accuracy')
+
+plt.fill_between(train_sizes,
+                 test_mean + test_std,
+                 test_mean - test_std,
+                 alpha=0.15, color='green')
+
+plt.grid()
+plt.xlabel('Number of training samples')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.ylim([0.8, 1.03])
+plt.tight_layout()
+# plt.savefig('images/06_05.png', dpi=300)
+plt.show()
+
+
+# 验证曲线
+from sklearn.model_selection import validation_curve
+
+
+param_range = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
+train_scores, test_scores = validation_curve(
+                estimator=pipe_lr,
+                X=X_train,
+                y=y_train,
+                param_name='logisticregression__C',
+                param_range=param_range,
+                cv=10)
+
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(param_range, train_mean,
+         color='blue', marker='o',
+         markersize=5, label='training accuracy')
+
+plt.fill_between(param_range, train_mean + train_std,
+                 train_mean - train_std, alpha=0.15,
+                 color='blue')
+
+plt.plot(param_range, test_mean,
+         color='green', linestyle='--',
+         marker='s', markersize=5,
+         label='validation accuracy')
+
+plt.fill_between(param_range,
+                 test_mean + test_std,
+                 test_mean - test_std,
+                 alpha=0.15, color='green')
+
+plt.grid()
+plt.xscale('log')
+plt.legend(loc='lower right')
+plt.xlabel('Parameter C')
+plt.ylabel('Accuracy')
+plt.ylim([0.8, 1.0])
+plt.tight_layout()
+# plt.savefig('images/06_06.png', dpi=300)
+plt.show()
