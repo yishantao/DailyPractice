d0ff5267b7bce85bd49fa09c92e5d8cf6933f1f3
yishantao <38241025+yishantao@users.noreply.github.com>
2018-07-09 19:22:18 +0800

    refactor

--- python_machine_learning/chapter6/chapter6.py
+++ python_machine_learning/chapter6/chapter6.py
@@ -54,95 +54,122 @@ import numpy as np
 # print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
 
 # 学习曲线函数评估模型
-import matplotlib.pyplot as plt
-from sklearn.model_selection import learning_curve
+# import matplotlib.pyplot as plt
+# from sklearn.model_selection import learning_curve
+#
+# pipe_lr = make_pipeline(StandardScaler(),
+#                         LogisticRegression(penalty='l2', random_state=1))
+#
+# train_sizes, train_scores, test_scores = learning_curve(estimator=pipe_lr,
+#                                                         X=X_train,
+#                                                         y=y_train,
+#                                                         train_sizes=np.linspace(0.1, 1.0, 10),
+#                                                         cv=10,
+#                                                         n_jobs=1)
+#
+# train_mean = np.mean(train_scores, axis=1)
+# train_std = np.std(train_scores, axis=1)
+# test_mean = np.mean(test_scores, axis=1)
+# test_std = np.std(test_scores, axis=1)
+#
+# plt.plot(train_sizes, train_mean,
+#          color='blue', marker='o',
+#          markersize=5, label='training accuracy')
+#
+# plt.fill_between(train_sizes,
+#                  train_mean + train_std,
+#                  train_mean - train_std,
+#                  alpha=0.15, color='blue')
+#
+# plt.plot(train_sizes, test_mean,
+#          color='green', linestyle='--',
+#          marker='s', markersize=5,
+#          label='validation accuracy')
+#
+# plt.fill_between(train_sizes,
+#                  test_mean + test_std,
+#                  test_mean - test_std,
+#                  alpha=0.15, color='green')
+#
+# plt.grid()
+# plt.xlabel('Number of training samples')
+# plt.ylabel('Accuracy')
+# plt.legend(loc='lower right')
+# plt.ylim([0.8, 1.03])
+# plt.tight_layout()
+# # plt.savefig('images/06_05.png', dpi=300)
+# plt.show()
+#
+# # 验证曲线
+# from sklearn.model_selection import validation_curve
+#
+# param_range = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
+# train_scores, test_scores = validation_curve(
+#     estimator=pipe_lr,
+#     X=X_train,
+#     y=y_train,
+#     param_name='logisticregression__C',
+#     param_range=param_range,
+#     cv=10)
+#
+# train_mean = np.mean(train_scores, axis=1)
+# train_std = np.std(train_scores, axis=1)
+# test_mean = np.mean(test_scores, axis=1)
+# test_std = np.std(test_scores, axis=1)
+#
+# plt.plot(param_range, train_mean,
+#          color='blue', marker='o',
+#          markersize=5, label='training accuracy')
+#
+# plt.fill_between(param_range, train_mean + train_std,
+#                  train_mean - train_std, alpha=0.15,
+#                  color='blue')
+#
+# plt.plot(param_range, test_mean,
+#          color='green', linestyle='--',
+#          marker='s', markersize=5,
+#          label='validation accuracy')
+#
+# plt.fill_between(param_range,
+#                  test_mean + test_std,
+#                  test_mean - test_std,
+#                  alpha=0.15, color='green')
+#
+# plt.grid()
+# plt.xscale('log')
+# plt.legend(loc='lower right')
+# plt.xlabel('Parameter C')
+# plt.ylabel('Accuracy')
+# plt.ylim([0.8, 1.0])
+# plt.tight_layout()
+# # plt.savefig('images/06_06.png', dpi=300)
+# plt.show()
 
-pipe_lr = make_pipeline(StandardScaler(),
-                        LogisticRegression(penalty='l2', random_state=1))
 
-train_sizes, train_scores, test_scores = learning_curve(estimator=pipe_lr,
-                                                        X=X_train,
-                                                        y=y_train,
-                                                        train_sizes=np.linspace(0.1, 1.0, 10),
-                                                        cv=10,
-                                                        n_jobs=1)
+# 网格搜索
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
 
-train_mean = np.mean(train_scores, axis=1)
-train_std = np.std(train_scores, axis=1)
-test_mean = np.mean(test_scores, axis=1)
-test_std = np.std(test_scores, axis=1)
+pipe_svc = make_pipeline(StandardScaler(),
+                         SVC(random_state=1))
 
-plt.plot(train_sizes, train_mean,
-         color='blue', marker='o',
-         markersize=5, label='training accuracy')
+param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
 
-plt.fill_between(train_sizes,
-                 train_mean + train_std,
-                 train_mean - train_std,
-                 alpha=0.15, color='blue')
+param_grid = [{'svc__C': param_range,
+               'svc__kernel': ['linear']},
+              {'svc__C': param_range,
+               'svc__gamma': param_range,
+               'svc__kernel': ['rbf']}]
+if __name__ == '__main__':
+    gs = GridSearchCV(estimator=pipe_svc,
+                      param_grid=param_grid,
+                      scoring='accuracy',
+                      cv=10,
+                      n_jobs=-1)
+    gs = gs.fit(X_train, y_train)
+    print(gs.best_score_)
+    print(gs.best_params_)
 
-plt.plot(train_sizes, test_mean,
-         color='green', linestyle='--',
-         marker='s', markersize=5,
-         label='validation accuracy')
-
-plt.fill_between(train_sizes,
-                 test_mean + test_std,
-                 test_mean - test_std,
-                 alpha=0.15, color='green')
-
-plt.grid()
-plt.xlabel('Number of training samples')
-plt.ylabel('Accuracy')
-plt.legend(loc='lower right')
-plt.ylim([0.8, 1.03])
-plt.tight_layout()
-# plt.savefig('images/06_05.png', dpi=300)
-plt.show()
-
-
-# 验证曲线
-from sklearn.model_selection import validation_curve
-
-
-param_range = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
-train_scores, test_scores = validation_curve(
-                estimator=pipe_lr,
-                X=X_train,
-                y=y_train,
-                param_name='logisticregression__C',
-                param_range=param_range,
-                cv=10)
-
-train_mean = np.mean(train_scores, axis=1)
-train_std = np.std(train_scores, axis=1)
-test_mean = np.mean(test_scores, axis=1)
-test_std = np.std(test_scores, axis=1)
-
-plt.plot(param_range, train_mean,
-         color='blue', marker='o',
-         markersize=5, label='training accuracy')
-
-plt.fill_between(param_range, train_mean + train_std,
-                 train_mean - train_std, alpha=0.15,
-                 color='blue')
-
-plt.plot(param_range, test_mean,
-         color='green', linestyle='--',
-         marker='s', markersize=5,
-         label='validation accuracy')
-
-plt.fill_between(param_range,
-                 test_mean + test_std,
-                 test_mean - test_std,
-                 alpha=0.15, color='green')
-
-plt.grid()
-plt.xscale('log')
-plt.legend(loc='lower right')
-plt.xlabel('Parameter C')
-plt.ylabel('Accuracy')
-plt.ylim([0.8, 1.0])
-plt.tight_layout()
-# plt.savefig('images/06_06.png', dpi=300)
-plt.show()
+    clf = gs.best_estimator_
+    clf.fit(X_train, y_train)
+    print('Test accuracy: %.3f' % clf.score(X_test, y_test))
