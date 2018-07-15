85b1b63308d9fa3a56d3e8fe8417e0b58c5cd2dc
yishantao <38241025+yishantao@users.noreply.github.com>
2018-07-15 16:24:49 +0800

    feat: knn

---
+++ python_machine_learning/chapter3/knn.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+
+"""K-nearest neighbors - a lazy learning algorithm"""
+
+from sklearn import datasets
+import matplotlib.pyplot as plt
+from plot_decision_regions import plot_decision_regions
+import numpy as np
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=1, stratify=y)
+
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=5,
+                           p=2,
+                           metric='minkowski')
+knn.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined,
+                      classifier=knn, test_idx=range(105, 150))
+
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
