72cd14113bd414e9b032605962ab83eb5a79f1ae
yishantao <38241025+yishantao@users.noreply.github.com>
2018-07-29 21:24:07 +0800

    refactor

--- gg.py
+++
@@ -1,8 +0,0 @@
-import tesserocr
-from PIL import Image
-
-image = Image.open('image.png')
-result = tesserocr.image_to_text(image)
-print(result)
-pip install tesserocr pillow
-pip install <package_name>.whl
